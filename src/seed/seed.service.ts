6e2ad5073d393f44235c7d84cb83b66a18808271
Daniel Richerson <richerdani3@gmail.com>
2022-11-17 17:55:42 -0500

    Api rest full

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { CarsService } from '../cars/cars.service';
+import { BrandsService } from '../brands/brands.service';
+import { CARS_SEED } from './data/car.seed';
+import { BRANDS_SEED } from './data/brands.seed';
 
 @Injectable()
 export class SeedService {
 
+  constructor( private readonly carsService: CarsService,
+               private readonly brandsService: BrandsService ) {}
+
   populateDB() {
+
+    this.carsService.fillCarsWithSeedData( CARS_SEED );
+    this.brandsService.fillBrandsWithSeedData( BRANDS_SEED );
+    
     return 'SEED execute suscessfully'
   }
 
